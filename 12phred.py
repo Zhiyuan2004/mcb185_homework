487b94891e86f7b0d22e6d2e010b25986743a6f2
Zhiyuan2004 <zyjjia@ucdavis.edu>
2025-02-04 11:29:04 -0800

    new

---
+++ 12phred.py
@@ -0,0 +1,19 @@
+print(ord('A'))
+print(ord('B'))
+print(ord('E'))
+print(ord('c'))
+
+print(chr(55))
+print(chr(56))
+print(chr(61))
+print(chr(62))
+print(chr(70))
+
+def char_to_prob(c):
+	return 0.001
+def prob_to_char(p):
+	return 'A'
+	
+print(char_to_prob('A'))
+print(char_to_prob('N'))
+print(prob_to_char(0.001))
