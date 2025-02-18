4e58f1c44bfa8ee0b36756e02d023985f3f14ae7
Zhiyuan2004 <zyjjia@ucdavis.edu>
2025-02-17 20:33:37 -0800

    new

--- au2.py
+++ au2.py
@@ -1,5 +1,15 @@
-for i in range(1,52):
-	if i % 2 == 1 and i % 3 == 1:
-		print(i,'*')
-		
+import random
+
+ins = 0
+tot = 0
+
+while True:
+	x = random.random()
+	y = random.random()
+
+	if x ** 2 + y ** 2 < 1:
+		ins += 1
+	tot += 1
+	pi = (ins/tot) * 4
+	print(pi)
 	
