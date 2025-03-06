5e265bed8f7bd3c545821b15d0600c5becdc5878
Zhiyuan2004 <zyjjia@ucdavis.edu>
2025-03-06 14:35:14 -0800

    new

--- sequence.py
+++ sequence.py
@@ -11,4 +11,15 @@ def revcomp(dna):
 		elif nt == 'T': rc.append('A')
 		else: 			rc.append('N')
 	return ''.join(rc)
-	
+
+def translate(dna):
+	aas = []
+	for i in range(0, len(dna), 3):
+		codon = dna[i:i+3]
+		if 	 codon == 'ATG': aas.append('M')
+		elif codon == 'TAA': aas.append('*')
+		elif codon == 'TAG': aas.append('*')
+		elif codon == 'TGA': aas.append('*')
+		else: 				 aas.append('X')
+	return ''.join(aas)
+ 
